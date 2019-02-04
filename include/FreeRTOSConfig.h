966c13633e0bb2109497bdcf88d39ece3ad5ff40
Ardelean Calin <ardelean.calin@outlook.com>
2019-02-04 21:35:16 +0200

    Got cooperative sheduling to work.

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -43,7 +43,7 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION 1
+#define configUSE_PREEMPTION 0 // Preemtive sheduling doesn't seem to work... TODO
 #define configSUPPORT_STATIC_ALLOCATION 0
 #define configSUPPORT_DYNAMIC_ALLOCATION 1
 #define configUSE_IDLE_HOOK 0
@@ -52,10 +52,10 @@
 #define configTICK_RATE_HZ ( (TickType_t)1000 )
 #define configMAX_PRIORITIES ( 4 )
 #define configMINIMAL_STACK_SIZE ( (unsigned short)50 )
-#define configTOTAL_HEAP_SIZE ( ( size_t )( 1800 ) )
+#define configTOTAL_HEAP_SIZE ( ( size_t )( 900 ) )
 #define configMAX_TASK_NAME_LEN ( 8 )
 #define configUSE_TRACE_FACILITY 0
-#define configUSE_16_BIT_TICKS 1
+#define configUSE_16_BIT_TICKS 0
 #define configIDLE_SHOULD_YIELD 1
 #define configTICK_VECTOR ( TIMER0_B0_VECTOR )
 
--- main.c
+++ main.c
@@ -6,15 +6,19 @@
 #include "task.h"
 
 /* Function prototypes */
-void vBlinkLEDTask( void* pvParameters );
+void vBlinkRedLEDTask( void* pvParameters );
+void vBlinkGreenLEDTask( void* pvParameters );
 static void prvSetupHardware( void );
 
 int main( void )
 {
     prvSetupHardware();
 
+    /* Two simple tasks */
     xTaskCreate(
-        vBlinkLEDTask, "Blinky", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
+        vBlinkRedLEDTask, "BlinkyR", configMINIMAL_STACK_SIZE, NULL, 1U, NULL );
+    xTaskCreate(
+        vBlinkGreenLEDTask, "BlinkyG", configMINIMAL_STACK_SIZE, NULL, 1U, NULL );
 
     vTaskStartScheduler();
 
@@ -27,13 +31,12 @@ int main( void )
 /* Do any necessary hardware initialization */
 static void prvSetupHardware( void )
 {
-    taskDISABLE_INTERRUPTS();
     // Stop WDT
     WDT_A_hold( WDT_A_BASE );
 }
 
-/* Simple blinker task. Blinks on-board LED every 0.5s */
-void vBlinkLEDTask( void* pvParameters )
+/* Simple blinker task. Blinks on-board Red LED every 0.6s */
+void vBlinkRedLEDTask( void* pvParameters )
 {
     // P1.0 as output
     GPIO_setAsOutputPin( GPIO_PORT_P1, GPIO_PIN0 );
@@ -41,6 +44,19 @@ void vBlinkLEDTask( void* pvParameters )
     for ( ;; )
     {
         GPIO_toggleOutputOnPin( GPIO_PORT_P1, GPIO_PIN0 );
-        vTaskDelay( (TickType_t)pdMS_TO_TICKS( 500 ) );
+        vTaskDelay( (TickType_t)pdMS_TO_TICKS( 300 ) );
+    }
+}
+
+/* Simple blinker task. Blinks on-board Green LED every 0.2s */
+void vBlinkGreenLEDTask( void* pvParameters )
+{
+    // P4.7 as output
+    GPIO_setAsOutputPin( GPIO_PORT_P4, GPIO_PIN7 );
+
+    for ( ;; )
+    {
+        GPIO_toggleOutputOnPin( GPIO_PORT_P4, GPIO_PIN7 );
+        vTaskDelay( (TickType_t)pdMS_TO_TICKS( 100 ) );
     }
 }
